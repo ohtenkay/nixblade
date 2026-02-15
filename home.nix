324e4292475238df2702c0038652ed57534624c0
Ondřej Hložek <100673686+ohtenkay@users.noreply.github.com>
2026-02-15 22:46:24 +0100

    Add home and flake

---
+++ home.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+    home = {
+        username = "ondrej";
+        homeDirectory = "/home/ondrej";
+        stateVersion = "25.11";
+    };
+
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            rebuild = "sudo nixos-rebuild switch --flake ~/nixblade";
+    };
+    };
+
+}
