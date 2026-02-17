d6215034f9e07826e926c3acf0d3a037b8143f9f
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-02-17 17:18:18 +0100

    feat: enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,8 @@
 
     nixfmt
     nixd
+
+    bluetui
   ];
 
   # For nixd, enables custom options
@@ -149,6 +151,9 @@
       };
     };
   };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
