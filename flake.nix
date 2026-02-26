c5a2f6270cd1256c58f34d592eb6260b038baff1
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-02-26 14:08:51 +0100

    feat: add flake-parts and import-tree inputs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    import-tree.url = "github:vic/import-tree";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
