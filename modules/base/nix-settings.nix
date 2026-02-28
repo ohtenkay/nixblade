026e83f9a6aa81ddc1450c220e5724e323790810
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-02-28 22:36:14 +0100

    feat: flutter, not yet working

--- modules/base/nix-settings.nix
+++ modules/base/nix-settings.nix
@@ -2,6 +2,7 @@
 {
   flake.modules.nixos.base = {
     nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.android_sdk.accept_license = true;
 
     nix.settings.experimental-features = [
       "nix-command"
---
+++ modules/dev/flutter.nix
@@ -0,0 +1,63 @@
+{ ... }:
+{
+  flake.modules.nixos.dev =
+    { pkgs, ... }:
+    let
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        cmdLineToolsVersion = "8.0";
+        platformToolsVersion = "35.0.2";
+        buildToolsVersions = [
+          "34.0.0"
+          "35.0.0"
+        ];
+        platformVersions = [
+          "34"
+          "35"
+        ];
+        # emulator = true;
+        includeSystemImages = true;
+        systemImageTypes = [ "google_apis_playstore" ];
+        abiVersions = [ "x86_64" ];
+        extraLicenses = [
+          "android-sdk-license"
+          "android-sdk-preview-license"
+          "android-googletv-license"
+          "android-sdk-arm-dbt-license"
+          "google-gdk-license"
+          "intel-android-extra-license"
+          "intel-android-sysimage-license"
+          "mips-android-sysimage-license"
+        ];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in
+    {
+      environment.systemPackages = with pkgs; [
+        flutter
+        dart
+        androidSdk
+        android-tools
+
+        # flutter linux desktop dependencies
+        clang
+        cmake
+        ninja
+        pkg-config
+        gtk3
+        glib
+        pcre2
+        libepoxy
+        xorg.libX11
+      ];
+
+      users.users.ondrej.extraGroups = [
+        "kvm"
+      ];
+
+      environment.variables = {
+        ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+        ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+        CHROME_EXECUTABLE = "${pkgs.brave}/bin/brave";
+      };
+    };
+}
