48247f549b88f3f1538d86af2495f043f624d023
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-03-01 20:33:43 +0100

    refactor: remove the infra folder and confusing helper function

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -1,7 +1,13 @@
+# Needed for flake-parts to work.
 { inputs, ... }:
 {
   imports = [
     # https://flake.parts/options/flake-parts-modules.html
     inputs.flake-parts.flakeModules.modules
+  ];
+
+  # Optional now, since I currently don't use any perSystem.
+  systems = [
+    "x86_64-linux"
   ];
 }
--- modules/hosts/nixblade.nix
+++ modules/hosts/nixblade.nix
@@ -1,22 +1,27 @@
 # Declares the nixblade NixOS configuration by composing named modules.
-{ config, ... }:
+{ config, inputs, ... }:
+let
+  nixos = config.flake.modules.nixos;
+in
 {
-  configurations.nixos.nixblade.module = {
-    imports = with config.flake.modules.nixos; [
-      base
-      desktop
-      dev
-      shell
-      stylix
-      nixblade-hardware
-      # nvidia
-      neovim
+  flake.nixosConfigurations.nixblade = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      nixos.base
+      nixos.desktop
+      nixos.dev
+      nixos.shell
+      nixos.stylix
+      nixos.nixblade-hardware
+      # nixos.nvidia
+      nixos.neovim
+
+      {
+        networking.hostName = "nixblade";
+        nixpkgs.hostPlatform = "x86_64-linux";
+
+        # https://nixos.org/nixos/options.html
+        system.stateVersion = "25.11";
+      }
     ];
-
-    networking.hostName = "nixblade";
-    nixpkgs.hostPlatform = "x86_64-linux";
-
-    # https://nixos.org/nixos/options.html
-    system.stateVersion = "25.11";
   };
 }
